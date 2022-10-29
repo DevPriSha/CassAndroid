6b25d49efe327751dc3e08853fbaa4f6e10d3ef5
Priya Sharma <77352100+DevPriSha@users.noreply.github.com>
2022-10-29 16:01:57 +0530

    Update chat history format

--- cassandroid.py
+++ cassandroid.py
@@ -1,32 +1,46 @@
 from transformers import AutoModelWithLMHead, AutoTokenizer
 import torch
 import discord
+import pandas as pd
+from variables import TOKEN_CASS
 
-tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-small')
-model = AutoModelWithLMHead.from_pretrained('output-small-final')
-ids = {}
-chat_history_ids = ""
+tokenizer = AutoTokenizer.from_pretrained('microsoft/DialoGPT-large')
+model = AutoModelWithLMHead.from_pretrained('cass-large-four')
+
+chat_history_ids = None
+
+df = pd.read_csv('history.csv')
+#create a dictionary with id as key and its frequency as value
+id_dict = {}
+for i in df['id']:
+    if i in id_dict:
+        id_dict[i] += 1
+    else:
+        id_dict[i] = 1
+
 
 class MyClient(discord.Client):
     def __init__(self):
         super().__init__()
 
     def query(self, msg, id):
+        global id_dict
         global chat_history_ids
-        if id not in ids:
-            ids[id] = 0
+        if id not in id_dict:
+            id_dict[id] = 0
             step = 0
-        elif ids[id] == 2:
-            ids[id] = 0
+        elif id_dict[id] == 2:
+            id_dict[id] = 0
             step = 0
         else:
-            ids[id] += 1
-            step = ids[id]
+            id_dict[id] += 1
+            step = id_dict[id]
         # encode the new user input, add the eos_token and return a tensor in Pytorch
         new_user_input_ids = tokenizer.encode(msg + tokenizer.eos_token, return_tensors='pt')
         bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
 
-    # generated a response while limiting the total chat history to 1000 tokens, 
+    # generated a response while limiting the total chat history to 1000 tokens,
+         
         chat_history_ids = model.generate(
             bot_input_ids, max_length=200,
             pad_token_id=tokenizer.eos_token_id,  
@@ -62,6 +76,10 @@ class MyClient(discord.Client):
             return
         # form query payload with the content of the message
         print(message.content)
+        #add message.author.id and message.content to a csv file
+        df = pd.read_csv('history.csv')
+        df = df.concat({'id': message.author.id, 'message': message.content}, ignore_index=True)
+        df.to_csv('history.csv', index=False)
 
         # while the bot is waiting on a response from the model
         # set the its status as typing for user-friendliness
@@ -79,12 +97,12 @@ class MyClient(discord.Client):
                 bot_response = 'Hmm... something is not right.'
 
         # send the model's response to the Discord channel
-        await message.channel.send(bot_response)
+        print(bot_response)
+        await message.reply(bot_response, mention_author=False)
 
 def main():
-    # DialoGPT-medium-joshua is my model name
     client = MyClient()
-    client.run('OTc5Njk0MTYzMzEyMzk0Mjgw.Go2ynL.A6oFBzV3topn9efMy1ABbmqP0yncc0pr0Ogwz0')
+    client.run(TOKEN_CASS)
 
 if __name__ == '__main__':
   main()
